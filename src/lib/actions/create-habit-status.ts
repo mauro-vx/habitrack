945cbc4fb10f568843e85767cafd00e64a6e2786
Mauro Vio <maurovio.dev@gmail.com>
2025-06-01 12:19:23 +0200

    Refactor habit status creation to clean up and optimize logic
    Removed outdated commented code and incorporated time-related filters using year, month, and week for retrieving habit statuses. Improved database query efficiency and ensured proper error handling for existing habit status checks.

--- src/lib/actions/create-habit-status.ts
+++ src/lib/actions/create-habit-status.ts
@@ -9,6 +9,7 @@ import { HabitState, Status } from "@/app/enums";
 import { authenticateUser } from "@/lib/supabase/authenticate-user";
 import { getWeekDateSeries } from "@/lib/utils";
 import { cookies } from "next/headers";
+import { TablesInsert } from "@/lib/supabase/database.types";
 
 export async function createHabitStatus(
   prevState: { status: Status; message: string } | null,
@@ -28,41 +29,28 @@ export async function createHabitStatus(
   const cookieStore = await cookies();
   const timezone = cookieStore.get("timezone")?.value || "Europe/Prague";
 
-  // const { data: existingStatus, error: dbError } = await authSupabase
-  //   .from("habit_statuses")
-  //   .select("*")
-  //   .eq("habit_id", payload.habitId)
-  //   .gte("status_date", payload.weekStartDate.toISOString())
-  //   .lte("status_date", endOfWeek(payload.weekStartDate, { weekStartsOn: 1 }).toISOString())
-  //   .eq("day_number", payload.dayNumber)
-  //   .maybeSingle();
-  //
-  // if (dbError) {
-  //   return { status: Status.DATABASE_ERROR, message: `Error fetching habit status: ${dbError.message}` };
-  // }
-  //
-  // if (existingStatus) {
-  //   return { status: Status.DATABASE_ERROR, message: "Habit status already exists." };
-  // }
-
-  // const newHabitStatus: TablesInsert<"habit_statuses"> = {
-  //   id: uuidv4(),
-  //   habit_id: payload.habitId,
-  //   day_number: payload.dayNumber,
-  //   status_date: payload.statusDate.toISOString(),
-  //   completion_count: payload.initialState === HabitState.DONE ? 1 : 0,
-  //   skipped_count: payload.initialState === HabitState.SKIP ? 1 : 0,
-  //   created_at: new Date().toISOString(),
-  //   updated_at: new Date().toISOString(),
-  // };
-
   const zonedDate = toZonedTime(payload.weekStartDate, timezone);
+  const { year, month, week } = getWeekDateSeries(zonedDate).current;
 
-  const {
-    current: { year, month, week },
-  } = getWeekDateSeries(zonedDate);
+  const { data: existingStatus, error: dbError } = await authSupabase
+    .from("habit_statuses")
+    .select("*")
+    .eq("habit_id", payload.habitId)
+    .eq("year_number", year)
+    .eq("month_number", month)
+    .eq("week_number", week)
+    .eq("day_number", payload.dayNumber)
+    .maybeSingle();
 
-  const newHabitStatus = {
+  if (dbError) {
+    return { status: Status.DATABASE_ERROR, message: `Error fetching habit status: ${dbError.message}` };
+  }
+
+  if (existingStatus) {
+    return { status: Status.DATABASE_ERROR, message: "Habit status already exists." };
+  }
+
+  const newHabitStatus: TablesInsert<"habit_statuses"> = {
     id: uuidv4(),
     habit_id: payload.habitId,
     year_number: year,
